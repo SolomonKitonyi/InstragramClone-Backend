3576854a4def4217d6ae5b849ba6e10fd25be5f8
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-26 16:01:42 +0300

    add changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+const User = mongoose.model("User");
 
 router.get("/", (req, res) => {
   res.send("Hello");
@@ -11,7 +13,27 @@ router.post("/signup", (req, res) => {
   if (!email || !name || !password) {
     return res.status(422).json({ error: "please add all fields" });
   }
-  res.status(200).json({ message: "Successfully posted" });
+  User.findOne({ email: email })
+    .then((saveduser) => {
+      if (saveduser)
+        return res.status(422).json({ error: "User already exists" });
+      const user = new User({
+        name,
+        email,
+        password,
+      });
+      user
+        .save()
+        .then((user) => {
+          res.json({ message: "Saved Successfully" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
