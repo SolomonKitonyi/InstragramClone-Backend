2029a35089d1c5eff9311ee10b4ed4caa9ff5da3
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-26 15:44:47 +0300

    add changes to auth route

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
 
-require('./models/user')
-
+require("./models/user");
+app.use(express.json());
 app.use(require("./routes/auth"));
 
-const {MONGOURL} = require("./keys")
+const { MONGOURL } = require("./keys");
 
-mongoose.connect(MONGOURL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-})
+mongoose.connect(MONGOURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
-mongoose.connection.on('connected',()=>{
-    console.log("Connected to MongoDB");
-})
-mongoose.connection.on('error',(error)=>{
-    console.log("Error connecting to MongoDB ",error);
-})
-
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (error) => {
+  console.log("Error connecting to MongoDB ", error);
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() =>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/",(req,res) =>{
-    res.send("Hello")
-})
+router.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-module.exports = router
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!email || !name || !password) {
+    return res.status(422).json({ error: "please add all fields" });
+  }
+  res.status(200).json({ message: "Successfully posted" });
+});
+
+module.exports = router;
